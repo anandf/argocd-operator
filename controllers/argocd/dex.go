8eb7c75747bcc3b4e84876dd3c3e72a7799c23b8
Anand Francis Joseph <anjoseph@redhat.com>
2026-09-12 23:39:33 +0530

    Removed changes for using build tags for custom expiry seconds for testing
    Signed-off-by: Anand Francis Joseph <anjoseph@redhat.com>

--- controllers/argocd/dex.go
+++ controllers/argocd/dex.go
@@ -47,6 +47,12 @@ func getDexServerTokenSecretName(cr *argoproj.ArgoCD) string {
 	return argoutil.GetSecretNameWithSuffix(cr, common.ArgoCDDefaultDexServiceAccountName+"-token")
 }
 
+// dexServerTokenRenewalThreshold is how much nominal lifetime may remain before we treat the Dex token
+// as due for renewal (ExpirySecs * ArgoCDDexServerTokenRenewalThresholdPercent / 100).
+func dexServerTokenRenewalThreshold() time.Duration {
+	return time.Duration(common.ArgoCDDexServerTokenExpirySecs*common.ArgoCDDexServerTokenRenewalThresholdPercent/100) * time.Second
+}
+
 // needsDexTokenRenewal returns true when the token is missing, unparseable, or within the renewal window.
 func needsDexTokenRenewal(secret *corev1.Secret) bool {
 	if secret == nil || secret.Data == nil {
@@ -103,7 +109,7 @@ func (r *ReconcileArgoCD) getDexOAuthClientSecret(cr *argoproj.ArgoCD) (*string,
 	}
 
 	// Request a new time-limited token via the TokenRequest API.
-	expirationSeconds := getTokenExpirySeconds()
+	expirationSeconds := common.ArgoCDDexServerTokenExpirySecs
 	tokenRequest, err := r.K8sClient.CoreV1().ServiceAccounts(cr.Namespace).CreateToken(
 		context.TODO(),
 		sa.Name,
